4f3d9332efecf9e5e10c0715726b65f5881494ad
MJ <arunbi@naver.com>
2024-11-19 15:29:25 +0900

    branch features/container first commit

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -1,11 +1,12 @@
-import { Button } from "@upleat-ui/ui/button";
-import styles from "./page.module.css";
+import { Button } from '@upleat-ui/ui/button';
+import styles from './page.module.css';
 
 export default function Home() {
   return (
     <div className={styles.page}>
       <h1>Hello U PLEAT</h1>
       <Button>Click me</Button>
+      <p style={{ visibility: 'hidden' }}>container branch 생성 완료</p>
     </div>
   );
 }
