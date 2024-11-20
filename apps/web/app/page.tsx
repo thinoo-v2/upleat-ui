3ef91aca80673d39f336445b8f16bb1ca25c0664
graphytime <graphytime@gmail.com>
2024-11-20 23:36:30 +0900

    core stack 연결

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -1,4 +1,6 @@
-import { Button } from '@upleat-ui/ui/button';
+import {Button} from '@upleat-ui/ui';
+import { Stack } from '@mantine/core';
+
 import styles from './page.module.css';
 
 export default function Home() {
@@ -6,7 +8,18 @@ export default function Home() {
     <div className={styles.page}>
       <h1>Hello U PLEAT</h1>
       <Button>Click me</Button>
-      <p style={{ visibility: 'hidden' }}>container branch 생성 완료</p>
+      <p>features/vStack branch 생성 완료</p>
+      <Stack
+        className={styles.stack}
+        bg="var(--mantine-color-body)"
+        align="stretch"
+        justify="center"
+        gap="md"
+      >
+      <Button>1</Button>
+      <Button>2</Button>
+      <Button>3</Button>
+    </Stack>
     </div>
   );
 }
